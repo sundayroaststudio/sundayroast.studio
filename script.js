721817777eb58115fafeede4c783763878b60428
sundayroaststudio <sundayroaststudio@gmail.com>
2026-09-05 12:36:42 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-
-const menu=document.querySelector('.menu');
-const nav=document.querySelector('.nav');
-if(menu&&nav){menu.addEventListener('click',()=>{nav.classList.toggle('open');menu.setAttribute('aria-expanded',nav.classList.contains('open'))})}
